7fe1df0aebffced1639c78f529042e8330eca6ac
Bara Konateh <bkonateh1@gmail.com>
2022-10-24 19:54:44 +0100

    Added chats

---
+++ client/components/Navigation/ProjectSidebar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProjectSidebar() {
+  return <div>ProjectSidebar</div>;
+}
--- client/components/Navigation/Sidebar.tsx
+++ client/components/Navigation/Sidebar.tsx
@@ -58,6 +58,17 @@ export default function Sidebar() {
             </h2>
           </li>
         </ul>
+        <hr className="my-5 border-stone-800" />
+        <ul>
+          <li>
+            <h2 className="flex justify-between items-center ml-1 font-semibold text-stone-500 text-sm rounded-lg">
+              Chats
+              <p className="text-red-500 font-bold px-2 rounded cursor-pointer hover:bg-stone-500">
+                +
+              </p>
+            </h2>
+          </li>
+        </ul>
         <div
           id="dropdown-cta"
           className="p-4 mt-6 bg-blue-50 rounded-lg "
@@ -84,7 +95,7 @@ export default function Sidebar() {
                 <path
                   fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="#a8a29e"
+                  clipRule="#a8a29e"
                 ></path>
               </svg>
             </button>
