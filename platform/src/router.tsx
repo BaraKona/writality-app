e5ae5d5549cc33c4b7a8989c4c6a712f3f00d7c6
Bara Kona <bkonateh1@gmail.com>
2023-03-09 01:11:04 +0000

    fix import for socket

--- platform/src/router.tsx
+++ platform/src/router.tsx
@@ -1,8 +1,8 @@
 import {
-  createBrowserRouter,
-  Link,
-  Navigate,
-  redirect,
+	createBrowserRouter,
+	Link,
+	Navigate,
+	redirect,
 } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Dashboard } from "./pages/Dashboard";
@@ -12,92 +12,92 @@ import { Sidebar } from "./components/Navigation";
 import { Chapter, Project } from "./pages/dashboard/project";
 import { PostsPage } from "./pages/dashboard/PostsPage";
 import {
-  Collaboration,
-  CollaborationChapter,
+	Collaboration,
+	CollaborationChapter,
 } from "./pages/dashboard/collaboration";
 import { Error } from "./pages/Error";
 import { FourOFour } from "./pages/404";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_API_URL as string);
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "*",
-    element: <FourOFour />,
-  },
-  {
-    path: "/auth/login",
-    element: <LoginPage />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/auth/register",
-    element: <RegisterPage />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/auth/reset",
-    element: <ResetPage />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <Sidebar>
-        <Dashboard />
-      </Sidebar>
-    ),
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard/posts",
-    element: (
-      <Sidebar>
-        <PostsPage />
-      </Sidebar>
-    ),
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard/project/:project",
-    element: (
-      <div className="h-screen w-screen flex flex-col">
-        <Sidebar>
-          <Project />
-        </Sidebar>
-      </div>
-    ),
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard/project/:project/chapter/:chapter",
-    element: (
-      <Sidebar>
-        <Chapter />
-      </Sidebar>
-    ),
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard/collaboration/:collaborationId",
-    element: (
-      <Sidebar>
-        <Collaboration socket={socket} />
-      </Sidebar>
-    ),
-    errorElement: <Error />,
-  },
-  {
-    path: "/dashboard/collaboration/:collaborationId/chapter/:chapterId",
-    element: (
-      <Sidebar>
-        <CollaborationChapter socket={socket} />
-      </Sidebar>
-    ),
-    errorElement: <Error />,
-  },
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "*",
+		element: <FourOFour />,
+	},
+	{
+		path: "/auth/login",
+		element: <LoginPage />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/auth/register",
+		element: <RegisterPage />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/auth/reset",
+		element: <ResetPage />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard",
+		element: (
+			<Sidebar>
+				<Dashboard />
+			</Sidebar>
+		),
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard/posts",
+		element: (
+			<Sidebar>
+				<PostsPage />
+			</Sidebar>
+		),
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard/project/:project",
+		element: (
+			<div className="h-screen w-screen flex flex-col">
+				<Sidebar>
+					<Project />
+				</Sidebar>
+			</div>
+		),
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard/project/:project/chapter/:chapter",
+		element: (
+			<Sidebar>
+				<Chapter />
+			</Sidebar>
+		),
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard/collaboration/:collaborationId",
+		element: (
+			<Sidebar>
+				<Collaboration socket={socket} />
+			</Sidebar>
+		),
+		errorElement: <Error />,
+	},
+	{
+		path: "/dashboard/collaboration/:collaborationId/chapter/:chapterId",
+		element: (
+			<Sidebar>
+				<CollaborationChapter socket={socket} />
+			</Sidebar>
+		),
+		errorElement: <Error />,
+	},
 ]);
