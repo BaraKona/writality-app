787e759258d4da3a1edd8562790cbd480360436f
Bara Kona <bkonateh1@gmail.com>
2023-03-12 23:32:49 +0000

    SEO Fix + border styling on list items

--- platform/src/components/ListItems/CommunityListItem.tsx
+++ platform/src/components/ListItems/CommunityListItem.tsx
@@ -1,24 +1,24 @@
 import React, { ReactNode, FC } from "react";
 import { useLocation } from "react-router-dom";
 export const CommunityListItem: FC<{
-  children: ReactNode;
-  name: string;
-  onClick?: () => void;
+	children: ReactNode;
+	name: string;
+	onClick?: () => void;
 }> = ({ name, onClick, children }) => {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  return (
-    <li onClick={onClick} className="cursor-default">
-      <a
-        className={`ml-3 flex text-md font-normal rounded  ${
-          pathname.includes(name.toLowerCase())
-            ? "text-purple-300 bg-base"
-            : "text-stone-300"
-        } hover:bg-base`}
-      >
-        {children}
-        <span className={`ml-3 `}>{name}</span>
-      </a>
-    </li>
-  );
+	return (
+		<li onClick={onClick} className="cursor-default">
+			<a
+				className={`ml-3 flex text-md font-normal rounded  ${
+					pathname.includes(name.toLowerCase())
+						? "text-purple-300 bg-base border border-baseBorderDark"
+						: "text-stone-300"
+				} hover:bg-base`}
+			>
+				{children}
+				<span className={`ml-3 `}>{name}</span>
+			</a>
+		</li>
+	);
 };
--- platform/src/components/ListItems/ProjectListItem.tsx
+++ platform/src/components/ListItems/ProjectListItem.tsx
@@ -3,34 +3,34 @@ import { AiFillFolder, AiFillFolderOpen } from "react-icons/ai";
 import { useParams } from "react-router-dom";
 
 export const ProjectListItem: FC<{
-  name: string;
-  projectId?: string;
-  onClick?: () => void;
+	name: string;
+	projectId?: string;
+	onClick?: () => void;
 }> = ({ name, onClick, projectId }) => {
-  const { project, collaborationId } = useParams();
-  return (
-    <>
-      {projectId === project || projectId === collaborationId ? (
-        <li
-          onClick={onClick}
-          className="p-[0.1rem] hover:bg-base bg-base cursor-default"
-        >
-          <a className="ml-3 flex text-md font-normal">
-            <AiFillFolderOpen size={20} color={"#b8a285"} />{" "}
-            <span className="ml-3 text-purple-300">{name}</span>
-          </a>
-        </li>
-      ) : (
-        <li
-          onClick={onClick}
-          className="p-[0.1rem] hover:bg-base cursor-default"
-        >
-          <a className="ml-3 flex text-md font-normal">
-            <AiFillFolder size={20} color={"#a8a29e"} />
-            <span className="ml-3">{name}</span>
-          </a>
-        </li>
-      )}
-    </>
-  );
+	const { project, collaborationId } = useParams();
+	return (
+		<>
+			{projectId === project || projectId === collaborationId ? (
+				<li
+					onClick={onClick}
+					className="p-[0.1rem] hover:bg-base bg-base cursor-default border border-baseBorderDark "
+				>
+					<a className="ml-3 flex text-md font-normal">
+						<AiFillFolderOpen size={20} color={"#b8a285"} />{" "}
+						<span className="ml-3 text-purple-300 ">{name}</span>
+					</a>
+				</li>
+			) : (
+				<li
+					onClick={onClick}
+					className="p-[0.1rem] hover:bg-base cursor-default border border-transparent hover:border-baseBorderDark"
+				>
+					<a className="ml-3 flex text-md font-normal">
+						<AiFillFolder size={20} color={"#a8a29e"} />
+						<span className="ml-3">{name}</span>
+					</a>
+				</li>
+			)}
+		</>
+	);
 };
