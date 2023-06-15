22bc55023b06d1278a0e800d0b38fa6da874ad0f
Bara Kona <bkonateh1@gmail.com>
2023-06-15 11:06:47 +0100

    filter posts by created

--- server/src/controllers/cPosts.ts
+++ server/src/controllers/cPosts.ts
@@ -1,49 +1,50 @@
 import Posts from "../models/postSchema";
 
 export const getPosts = async (req: any, res: any) => {
-  try {
-    const posts = await Posts.find({
-      limit: 25,
-    });
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+	try {
+		const posts = await Posts.find({
+			limit: 25,
+			sort: { dateCreated: -1 },
+		});
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
 };
 
 export const createPost = async (req: any, res: any) => {
-  const {
-    owner,
-    postTitle,
-    projectTitle,
-    description,
-    genres,
-    postType,
-    likes,
-    dateCreated,
-    comments,
-    collaborationType,
-    collaboration,
-  } = req.body;
-  const newPost = new Posts({
-    owner,
-    postTitle,
-    projectTitle,
-    description,
-    genres,
-    postType,
-    likes,
-    dateCreated,
-    comments,
-    collaborationType,
-    collaboration,
-  });
-  try {
-    await newPost.save();
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(409).json({
-      message: "Something went wrong, we could not get create your post",
-    });
-  }
+	const {
+		owner,
+		postTitle,
+		projectTitle,
+		description,
+		genres,
+		postType,
+		likes,
+		dateCreated,
+		comments,
+		collaborationType,
+		collaboration,
+	} = req.body;
+	const newPost = new Posts({
+		owner,
+		postTitle,
+		projectTitle,
+		description,
+		genres,
+		postType,
+		likes,
+		dateCreated,
+		comments,
+		collaborationType,
+		collaboration,
+	});
+	try {
+		await newPost.save();
+		res.status(201).json(newPost);
+	} catch (error) {
+		res.status(409).json({
+			message: "Something went wrong, we could not get create your post",
+		});
+	}
 };
