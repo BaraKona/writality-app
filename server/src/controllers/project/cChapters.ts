4dc9c3f03f42fec4118f52c79708b18ec7e49b16
barakona <bkonateh1@gmail.com>
2023-07-31 18:43:55 +0100

    fix chapter issue

--- server/src/controllers/project/cChapters.ts
+++ server/src/controllers/project/cChapters.ts
@@ -114,7 +114,7 @@ export const updateChapterContent = async (req: any, res: any) => {
 
 		Promise.all([
 			Version.create({
-				title: chapter.content.title,
+				title: chapter.title,
 				owner: userId,
 				projectId: projectId,
 				chapterId: chapterId,
