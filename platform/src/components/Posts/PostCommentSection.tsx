650c0de528e6a072ba4cc3c8e901b5f9f476d7c3
Bara Kona <bkonateh1@gmail.com>
2023-11-04 19:51:10 +0100

    Fix spacing and textarea disappearing on scroll

--- platform/src/components/Posts/PostCommentSection.tsx
+++ platform/src/components/Posts/PostCommentSection.tsx
@@ -38,40 +38,40 @@ export const PostCommentSection: FC<{ post: IPost }> = ({ post }) => {
 			</div>
 
 			<Divider className="!border-coolGrey-1 dark:!border-borderDark" my={10} />
-			<div className="h-[calc(100vh-13rem)] overflow-y-auto  flex-grow-1 flex flex-col justify-between">
-				<div className="px-2">
+			<div className=" flex-grow-1 flex flex-col justify-between -mx-2">
+				<div className="h-[calc(100vh-17rem)] overflow-y-auto px-4 pl-2">
 					<ReadMoreText
 						text={post?.collaboration}
 						maxTextLength={200}
 						errorText="This post has no message to collaborators"
 					/>
-				</div>
-				<div className="gap-2 mt-3 flex flex-col justify-between ">
-					<div ref={parent} className="overflow-y-auto flex-grow mt-auto">
-						{post?.comments?.map((comment) => (
-							<PostComment comment={comment} owner={post.owner._id} />
-						))}
+					<div className="gap-2 mt-3 flex flex-col justify-between ">
+						<div ref={parent} className="overflow-y-auto flex-grow mt-auto">
+							{post?.comments?.map((comment) => (
+								<PostComment comment={comment} owner={post.owner._id} />
+							))}
+						</div>
 					</div>
-					<Textarea
-						placeholder="Your comment"
-						className="mt-auto"
-						variant="default"
-						size="sm"
-						styles={inputStyles}
-						value={comment}
-						onChange={(event) => setComment(event.currentTarget.value)}
-						rightSection={
-							<IconSend
-								size={18}
-								className="text-coolGrey-7 hover:text-black dark:hover:text-coolGrey-1 cursor-pointer"
-								onClick={() => {
-									mutate(comment);
-									setComment("");
-								}}
-							/>
-						}
-					/>
 				</div>
+				<Textarea
+					placeholder="Your comment"
+					className="mt-auto"
+					variant="default"
+					size="sm"
+					styles={inputStyles}
+					value={comment}
+					onChange={(event) => setComment(event.currentTarget.value)}
+					rightSection={
+						<IconSend
+							size={18}
+							className="text-coolGrey-7 hover:text-black dark:hover:text-coolGrey-1 cursor-pointer"
+							onClick={() => {
+								mutate(comment);
+								setComment("");
+							}}
+						/>
+					}
+				/>
 			</div>
 		</div>
 	);
