d35caae47ec4627643a17a1e9b3f00751fd1fff4
Bara Kona <bkonateh1@gmail.com>
2023-11-15 02:14:05 +0100

    fix

--- platform/src/pages/post/PostComment.tsx
+++ platform/src/pages/post/PostComment.tsx
@@ -11,8 +11,8 @@ export const PostComment: FC<{
 	const isOwner = comment?.owner._id === owner;
 
 	return (
-		<div className="flex gap-2 mb-2 grow-1">
-			<div className="w-14 h-14 rounded-full !bg-coolGrey-2/70 dark:!bg-coolGrey-5/70 dark:bg-borderDark flex items-center justify-center border-[10px] dark:border-baseDark border-base shrink-0">
+		<div className="flex gap-2 mb-2 grow-1 dark:bg-hoverDark/40 rounded-lg p-1 bg-coolGrey-1 pr-4">
+			<div className="w-12 h-12 rounded-full !bg-coolGrey-2/70 dark:!bg-coolGrey-5/70 dark:bg-borderDark flex items-center justify-center border-[5px] dark:border-baseDark border-base shrink-0">
 				<div
 					className={`text-base font-bold truncate -mt-1 ${initialsColor(
 						comment?.owner.name
@@ -21,13 +21,13 @@ export const PostComment: FC<{
 					{initials(comment?.owner.name)}
 				</div>
 			</div>
-			<div className="flex-col flex mt-2 ">
+			<div className="flex-col flex mt-2 w-full">
 				<div className="flex gap-2">
 					{isOwner && <SmallText className="text-pink-500">[Owner]</SmallText>}
 					<SmallText className="text-coolGrey-7 dark:text-coolGrey-3">
 						{comment?.owner.name}
 					</SmallText>
-					<SmallText className="!text-cyan-700 dark:text-coolGrey-5">
+					<SmallText className="!text-cyan-700 ml-auto">
 						{useTimeFromNow(comment?.dateCreated.toString())}
 					</SmallText>
 				</div>
