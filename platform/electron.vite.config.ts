4cae88b0bdee38207e4c9963d08fcf278b3662f1
Bara Kona <bkonateh1@gmail.com>
2023-12-21 01:43:22 +0100

    add electron

---
+++ platform/electron.vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'electron-vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    root: '.',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'index.html')
+        }
+      }
+    },
+    plugins: [react()]
+  }
+})
--- platform/vite.config.ts
+++ platform/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
 	base: "./platform/",
 	server: {
 		host: true,
+	},
+	resolve: {
+		alias: {
+			"@": path.join(__dirname, "src"),
+		},
 	},
 });
