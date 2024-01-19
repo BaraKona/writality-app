e951e1d21c5a9ec7c7cc16ff354f418102ad84fd
Bara Kona <bkonateh1@gmail.com>
2024-01-19 23:10:33 +0100

    Remove pluginPurgeCss from vite.config.ts

--- platform/vite.config.ts
+++ platform/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
-import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), pluginRewriteAll(), pluginPurgeCss()],
+	plugins: [react(), pluginRewriteAll()],
 
 	build: {
 		chunkSizeWarningLimit: 1000,
@@ -51,7 +50,6 @@ export default defineConfig({
 			},
 		},
 	},
-	base: "./platform/",
 	server: {
 		host: true,
 	},
