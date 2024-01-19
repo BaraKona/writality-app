d16a1cc5dcffbc840d9ba809cbfdab89e6caabd9
Bara Kona <bkonateh1@gmail.com>
2024-01-19 15:12:08 +0100

    Resolve url issues

--- platform/vite.config.ts
+++ platform/vite.config.ts
@@ -5,7 +5,7 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), pluginRewriteAll()],
-	base: "./platform/",
+	base: "./",
 	server: {
 		host: true,
 	},
